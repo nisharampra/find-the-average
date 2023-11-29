204d2090f9381dc33e8e8822f1a34e8e8736fb99
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-11-29 16:12:35 +0800

    Add files via upload

---
+++ find the average/find the average/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  find the average
+//
+//  Created by Nisha Ramprasath on 1/11/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    float num[100], sum=0.0, average;
+
+    cout << "Enter the Numbers of Element: ";
+    cin >> n;
+
+    for(i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Enter number: ";
+        cin >> num[i];
+        sum += num[i];
+    }
+
+    average = sum / n;
+    cout << "Average = " << average;
+
+    return 0;
+        
+}
+    
